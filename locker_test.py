80e905243403e170b9e7c319e57c197ae967fea2
kema-ray <rachelkemuma99@gmail.com>
2022-04-23 14:01:21 +0300

    append a test to save multiple users

--- locker_test.py
+++ locker_test.py
@@ -1,3 +1,4 @@
+from cgi import test
 import unittest
 from locker import User
 
@@ -10,18 +11,29 @@ class TestUser(unittest.TestCase):
         set up method to run before each test cases.
         '''
         self.new_user = User("RachelOyondi","kema99Ray")
+
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
         self.assertEqual(self.new_user.username,"RachelOyondi")
         self.assertEqual(self.new_user.password,"kema99Ray")
+
     def test_save_user(self):
         '''
         test_save_user teat case to test if the user object is saved into
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","qwerty2u")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
 
 if __name__=='__main__':
     unittest.main()
